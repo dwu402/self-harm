f5760bee16a5dcef8f54569b6745b3d389460955
David Wu <dwu402@aucklanduni.ac.nz>
2018-11-09 10:21:18 +1300

    TEST: force debugging of the esr scenario

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main(model_file, parameter_file):
     model_function = model.get_model(model_file)
     model_context = model.get_context(parameter_file)
     model_results = model.integrate_model(model_function, model_context)
-    
+
     fitter.plot_results(model_results['y'])
 
 if __name__ == "__main__":
-    main()
+    main(["-f", "models/esr_model.py", "-p", "parameters/esr_parameters.txt"])

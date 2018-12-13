a608eb00edcbace1590564cebe6d804655cad26a
David Wu <dwu402@aucklanduni.ac.nz>
2018-12-13 16:36:57 +1300

    Rewrite the results visualiser for new output format

--- view.py
+++ view.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python
 """Helper application that views results"""
-import glob
 import warnings
-from pathlib import Path
 import click
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 
 warnings.filterwarnings("error")
 
-def read_directory(directory, data):
-    appended = data
-    for outfile in glob.glob(directory + "/*.out"):
-        file_result = pd.read_csv(outfile, header=None, squeeze=True)
-        named_result = pd.DataFrame({Path(outfile).stem: file_result})
-        appended = pd.concat([appended, named_result], axis=1)
-    return appended
+
+def parse_outfile(file_name):
+    with open(file_name) as outfile:
+        runs = filter(None, outfile.read().split('---'))
+        results = [filter(None, p.rstrip().split('\nf ')) for p in runs]
+        parameters = [[float(p) for p in ps] for ps in results]
+    return parameters
 
 
-def show_results(data, axis):
+def bar_results(data, axis):
     N = len(data)
     xs = np.arange(N)
-    cols = len(data.columns)
+    cols = len(data[0])
     width = 1/(cols+1)
 
     for idx in range(cols):
-        ps = np.abs(data.iloc[:, idx])
+        ps = data[idx]
         axis.bar(xs+width*idx, ps, width)
 
     axis.set_xticks(xs+width*cols/2)
     axis.set_xticklabels(xs)
 
 
+def box_results(data, axis):
+    transposed_data = [list(i) for i in zip(*data)]
+    axis.boxplot(transposed_data)
+
+
+def show_results(plot_type, data, axis):
+    if plot_type == 'bar':
+        bar_results(data, axis)
+    elif plot_type =='box':
+        box_results(data, axis)
+    else:
+        raise TypeError('Plot type' + str(type) + 'not recognised')
+
+
 @click.command()
-@click.option('-d', '--results-directory', help='path to directory that holds the results')
-def main(results_directory):
-    data = pd.DataFrame()
-    data = read_directory(results_directory, data)
+@click.option('-f', '--results-file', help='path to file that holds the results')
+@click.option('-t', '--plot-type', default='box' , help='type of plot')
+def main(results_file, plot_type):
+    data = parse_outfile(results_file)
     fig, ax = plt.subplots()
-    show_results(data, ax)
+    show_results(plot_type, data, ax)
     plt.show()
 
 if __name__ == "__main__":

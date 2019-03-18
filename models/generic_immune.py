3ce17a84fbff193e1b909d65913a8d10be158152
David Wu <dwu402@aucklanduni.ac.nz>
2019-03-18 19:07:01 +1300

    Add the generic immune model

---
+++ models/generic_immune.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def model(t, state, parameters):
+
+    x, y, z = state
+    r, k, p, s, d, f, g, j, l = np.fabs(parameters)
+
+    return [
+        r*x - k*x*y,
+        p*x/(1+x**2) + s*(y**3)/(1+y**3) - d*y,
+        f*y*z - g*z - j*x - l*y
+    ]
+
+def model_form():
+    return {
+        "state": 3,
+        "parameters": 9,
+    }

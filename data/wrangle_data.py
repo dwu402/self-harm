98c7fcb44872c7a91847ee972931a93ed9b4194d
David Wu <dwu402@aucklanduni.ac.nz>
2019-03-18 19:07:57 +1300

    Add configs and data downloader

---
+++ data/wrangle_data.py
@@ -0,0 +1,20 @@
+"""Data wrangling for the default sources"""
+import pandas as pd
+from pathlib import Path
+import click
+
+
+@click.command()
+@click.option('-f', '--file-path', help='path to file')
+def main(file_path):
+    file_path = Path(file_path)
+    data_contents = pd.read_excel(file_path)
+    key_column = data_contents['Mouse']
+    for key in key_column.unique():
+        subfile_path = file_path.parent.joinpath('mouse' + str(key) + '.csv')
+        data_subsection = data_contents[key_column == key]
+        data_subsection.to_csv(subfile_path, mode='w', index=False)
+
+
+if __name__ == "__main__":
+    main() # pylint: disable=no-value-for-parameter
